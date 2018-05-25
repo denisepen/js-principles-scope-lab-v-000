b9092616885f2bf9b38d5bd9b199a069de2e24c7
Denise Pendleton <dpendl1@hotmail.com>
2018-05-25 15:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function setBestCustomer(){
   bestCustomer = "not bob";
 }
 
-// overwriteBestCustomer(){
-//   bestCustomer = "francie"
-// }
+function overwriteBestCustomer(){
+  bestCustomer = "francie"
+}
